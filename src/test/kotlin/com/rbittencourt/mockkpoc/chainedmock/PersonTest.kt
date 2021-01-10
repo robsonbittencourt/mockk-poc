c6dc65750147943cd083da77e32d8cc0e2c054ec
robsonbittencourt <robson.luizv@gmail.com>
2021-01-10 15:00:42 -0300

    Add text to chained mocks

--- src/test/kotlin/com/rbittencourt/mockkpoc/chainedmock/PersonTest.kt
+++ src/test/kotlin/com/rbittencourt/mockkpoc/chainedmock/PersonTest.kt
@@ -8,6 +8,16 @@ import org.junit.jupiter.api.Test
 class PersonTest {
 
     @Test
+    fun `should return mocked address`() {
+        val person = mockk<Person>()
+
+        val street = "Central Park West"
+        every { person.address } returns Address(street, 20)
+
+        assertEquals(street, person.address.street)
+    }
+
+    @Test
     fun `should return mocked address number using chained mock`() {
         val person = mockk<Person>()
 
